c18e5941d62971709572a6bb8362e36fcc64a9f8
FedericoBiderman <129424077+FedericoBiderman@users.noreply.github.com>
2024-03-26 09:52:35 -0300

    Add files via upload

---
+++ TP2-Biderman-Bursztyn/index.js
@@ -0,0 +1,9 @@
+class item {
+    constructor(sentence, start, end, status  ) {
+      this.sentence = sentence;
+      this.start = start;
+      this.end = end;
+      this.status = status;
+    }
+  }
+  
